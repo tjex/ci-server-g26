c7807ceda1ba49d60c7918fd4da32a66bad74f32
Arian Hanifi <counterstrike1337@hotmail.com>
2023-02-09 22:59:17 +0100

    refactor: merge two tests since cloning takes long time on rpi #45

--- src/test/java/com/group26/MainTest.java
+++ src/test/java/com/group26/MainTest.java
@@ -44,7 +44,7 @@ public class MainTest
      * Tests if a working commit on main is built successful
      * The specific commit a6c479e2db14d78bf5270701c57697b544c74bba has been tested manually
     **/
-    public void testBuildSuccess() throws IOException, InterruptedException {
+    public void testBuildSuccessAndFail() throws IOException, InterruptedException {
         String path = "/home/g26/testing/";
         File dir = new File(path);
         try {
@@ -60,7 +60,6 @@ public class MainTest
             throw new RuntimeException(e);
         }
 
-
         try{
             ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", "cd " + path + " && git checkout a6c479e2db14d78bf5270701c57697b544c74bba");
             Process proc = processBuilder.start();
@@ -72,43 +71,7 @@ public class MainTest
 
         boolean succes = server.buildRepo(path);
         assertEquals(true, succes);
-    }
 
-    /**
-     * Tests if a build fails
-     * By removing the file that contains the function that is used in this test
-     * the project should not be able to build
-    **/
-    public void testBuildFail() throws IOException, InterruptedException {
-        String path = "/home/g26/testing/";
-        System.out.println("directory to be deleted " + path);
-        File dir = new File(path);
-        try {
-            FileUtils.deleteDirectory(dir);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        ContinuousIntegrationServer server = new ContinuousIntegrationServer();
-        try {
-            HelperFucntion.gitClone("https://github.com/tjex/ci-server-g26.git", "main", path);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        // checking out a commit that passes all text
-        try{
-            ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", "cd " + path + " && git checkout a6c479e2db14d78bf5270701c57697b544c74bba");
-            Process proc = processBuilder.start();
-            proc.waitFor();
-        }
-        catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        // Deleting the file that contains the function that's called in this function should give build error
         File file = new File(path + "src/main/java/org/group26/HelperFucntion.java");
         System.out.println(path + "src/main/java/org/group26/HelperFucntion.java");
         Boolean is_deleted = file.delete();
